c7f238fa3558b339faf763517b21a00807d5977c
govindleo_11 <govindaragavan10@gmail.com>
2026-08-21 01:42:22 +0530

    LC-make-the-string-great [Runtime: N/A; Memory: N/A]

--- string/make-the-string-great.cpp
+++ string/make-the-string-great.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     string makeGood(string s) {
-     stack<char> st;
-     int n = s.length();
-     for(int i=0;i<n;i++){
-        if(isupper(s[i])){
-        if(!st.empty() && toupper(st.top())==s[i]){
-        while(!st.empty() && toupper(st.top())==s[i])
-        st.pop();}
-        else
-        st.push(s[i]);
+        stack<char> st;
+        for (char c : s) {
+            if (!st.empty() && abs(st.top() - c) == 32) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
         }
-else if(islower(s[i])){
-        if(!st.empty() && st.top()==toupper(s[i])){
-        while(!st.empty() && st.top()==toupper(s[i]))
-        st.pop();}
-        else
-        st.push(s[i]);
+        string res;
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
         }
-     }
-    string res= "";
-    while(!st.empty()){
-        res+=st.top();
-        st.pop();
-    } 
-    reverse(res.begin(),res.end()); 
-    return res;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
