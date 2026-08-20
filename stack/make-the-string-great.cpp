cf05d8a91e762bf91aee28463a52173f979f0612
govindleo_11 <govindaragavan10@gmail.com>
2026-08-21 01:40:46 +0530

    LC-make-the-string-great [Runtime: N/A; Memory: N/A]

--- stack/make-the-string-great.cpp
+++ stack/make-the-string-great.cpp
@@ -5,11 +5,19 @@ public:
      int n = s.length();
      for(int i=0;i<n;i++){
         if(isupper(s[i])){
-        while(!st.empty()  && toupper(st.top())==s[i])
-        st.pop();
-        }
+        if(!st.empty() && toupper(st.top())==s[i]){
+        while(!st.empty() && toupper(st.top())==s[i])
+        st.pop();}
         else
         st.push(s[i]);
+        }
+else if(islower(s[i])){
+        if(!st.empty() && st.top()==toupper(s[i])){
+        while(!st.empty() && st.top()==toupper(s[i]))
+        st.pop();}
+        else
+        st.push(s[i]);
+        }
      }
     string res= "";
     while(!st.empty()){
