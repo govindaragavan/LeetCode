c2c4cd3167a830f667734a1c62b91ef1ee436361
govindleo_11 <govindaragavan10@gmail.com>
2026-08-21 15:03:14 +0530

    LC-next-greater-node-in-linked-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/next-greater-node-in-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        ListNode * temp= head;
+        vector<int> list;
+        while(temp){
+        list.push_back(temp->val) ;
+        temp=temp->next;
+        }
+        stack<int> st;
+        int n=list.size();
+        vector<int> nge(n);
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && list[i]<st.top())
+            st.pop();
+            
+            nge[i]=st.empty() ? 0 : st.top();
+
+            st.push(list[i]);
+        }
+        return nge;
+    }
+};
