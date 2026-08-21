8047263bce800c3920077baafcdde5a60ca2d9d5
govindleo_11 <govindaragavan10@gmail.com>
2026-08-21 17:59:52 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- prefix-sum/product-of-array-except-self.cpp
+++ prefix-sum/product-of-array-except-self.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> res;
-        long long a[n],b[n];
-        a[0]=nums[0];
-        b[n-1]=nums[n-1];
-        for(int i=1;i<n;i++)
-        a[i]=(nums[i]*a[i-1]);
-        
-        for(int i=nums.size()-2;i>=0;i--)
-        b[i]=(nums[i]*b[i+1]);
-        
-            for(int i=0;i<n;i++){
-            if(i==0)
-            res.push_back(b[1]);
-            else if(i==nums.size()-1)
-            res.push_back(a[n-2]);
-            else
-            res.push_back(a[i-1]*b[i+1]);
+        int n = nums.size();
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        int prod=1;
+        for(int i=0;i<n;i++){
+            prod*=nums[i];
+            prefix[i]=prod;
         }
-        return res;
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            prod*=nums[i];
+            suffix[i]=prod;
+        }
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+         if(i==0)
+         a[i]=suffix[i+1];
+         else if(i==n-1)
+         a[i]=prefix[i-1];
+         else {
+          int left=prefix[i-1];
+          int right=suffix[i+1];
+          a[i]=left*right;}
+        }
+        return a;
     }
 };
