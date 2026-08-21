11928d2617bd6e198a325d4e0cdf9f829107acc2
govindleo_11 <govindaragavan10@gmail.com>
2026-08-21 14:44:34 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- string/backspace-string-compare.cpp
+++ string/backspace-string-compare.cpp
@@ -6,13 +6,13 @@ public:
         for(char c : s){
         if(!st1.empty() && c=='#')
         st1.pop_back();
-        else
+        else  if(c!='#')
         st1.push_back(c);
         }
         for(char c : t){
         if(!st2.empty() && c=='#')
         st2.pop_back();
-        else
+        else if(c!='#')
         st2.push_back(c);
         }
         if(st1!=st2)
