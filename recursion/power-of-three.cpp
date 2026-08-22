e8ce327fcd40b00fb677941ea76b67e308de5dba
govindleo_11 <govindaragavan10@gmail.com>
2026-08-22 12:26:34 +0530

    LC-power-of-three [Runtime: N/A; Memory: N/A]

--- recursion/power-of-three.cpp
+++ recursion/power-of-three.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)
-        return false;
-        if(n==1)
-        return true;
-        if(isPowerOfThree(n/3))
-        return true;
-        return false;
-    }
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%3!=0) return false;
+        return isPowerOfThree(n/3);    }
 };
