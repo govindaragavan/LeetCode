99c1ee53859e36581f0724c036d15251c3116934
govindleo_11 <govindaragavan10@gmail.com>
2026-08-22 12:13:39 +0530

    LC-power-of-two [Runtime: N/A; Memory: N/A]

--- recursion/power-of-two.cpp
+++ recursion/power-of-two.cpp
@@ -4,7 +4,7 @@ public:
         if(n!=1 && n%2!=0) return false;
         if(n==1)
         return true;
-       if( isPowerOfTwo(n/2));
+       if( isPowerOfTwo(n/2))
        return true;
         return false;
     }
