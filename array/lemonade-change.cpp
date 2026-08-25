84eba670dbf286cc2e44cb3062b4817b48297d19
govindleo_11 <govindaragavan10@gmail.com>
2026-08-25 19:53:57 +0530

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

--- array/lemonade-change.cpp
+++ array/lemonade-change.cpp
@@ -10,7 +10,7 @@ public:
                 if(five == 0) return false;
                 five--;
                 ten++;
-            } else { // bill == 20
+            } else { 
                 if(ten > 0 && five > 0) {
                     ten--;
                     five--;
