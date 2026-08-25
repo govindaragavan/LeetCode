15cc84ef856bad041aa65cbc85c7bb08cb0ae833
govindleo_11 <govindaragavan10@gmail.com>
2026-08-25 19:16:21 +0530

    LC-longest-substring-without-repeating-characters [Runtime: 59 ms; Memory: 19.3 MB]

--- hash-table/longest-substring-without-repeating-characters.cpp
+++ hash-table/longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,11 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.length();
         unordered_map<char,int> mp;
-        int l=0,ans=1;
+        int l=0,ans=0;
         for(int i=0;i<n;i++){
         mp[s[i]]++;
         while(mp[s[i]]>1){
-          if(s[l]==s[i])
-          mp[s[i]]--;
-
+          mp[s[l]]--;
           l++;
         }
 
