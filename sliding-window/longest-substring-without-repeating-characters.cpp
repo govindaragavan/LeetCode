ba97cd36cd3b284103776d2a64c5004815e924e6
govindleo_11 <govindaragavan10@gmail.com>
2026-08-25 19:07:57 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-substring-without-repeating-characters.cpp
+++ sliding-window/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s=="")
-        return 0;
+        int n = s.length();
         unordered_map<char,int> mp;
-        int l=0,r=0;
-        int ans=INT_MIN;
-        int len=0;
-       while(r<s.length()){
-        mp[s[r]]++;
-        while(mp[s[r]]>1){
-            mp[s[l]]--;
-            l++;
-            len--;
+        int l=0,ans=1;
+        for(int i=0;i<n;i++){
+        mp[s[i]]++;
+        while(mp[s[i]]>1){
+          if(s[l]==s[i])
+          mp[s[i]]--;
+
+          l++;
         }
-        len++;
-        ans=max(ans,len);
-        r++;
-       }
-       return ans;
+
+        ans=max(ans,i-l+1);
+        }
+        return ans;
     }
 };
