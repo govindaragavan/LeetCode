eb5fde0705fdf1ce04a6ee7c79d85ffef77304fc
govindleo_11 <govindaragavan10@gmail.com>
2026-08-25 19:35:09 +0530

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- array/assign-cookies.cpp
+++ array/assign-cookies.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = s.size(),m=g.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n = g.size(),m=s.size();
       int i=0,j=0;
       while(i<n && j<m){
-            if(s[i]>=g[j]){
-     i++;
-  }
+            if(s[j]>=g[i]){
+          i++;
+       }
         j++;
       }
         return i;
