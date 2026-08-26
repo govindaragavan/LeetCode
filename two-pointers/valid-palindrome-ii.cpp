a552ee3c54f0e27b1c9e4f91c935552d11569051
govindleo_11 <govindaragavan10@gmail.com>
2026-08-26 14:30:06 +0530

    LC-valid-palindrome-ii [Runtime: 3 ms; Memory: 22.2 MB]

--- two-pointers/valid-palindrome-ii.cpp
+++ two-pointers/valid-palindrome-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
+    bool isPalindrome(const string &s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+    
     bool validPalindrome(string s) {
-        int n=s.length();
-        unordered_map<char,int> mp;
-        for(int i=0;i<n;i++)
-        mp[s[i]]++;
-        int odd=0;
-       int sumofodd=0;
-     for(auto it=mp.begin();it!=mp.end();++it){
-        if(it->second%2!=0){
-          odd++;
-          sumofodd+=it->second;}
-          cout<<it->second<<" ";
-     }
-    cout<<odd<<" "<<sumofodd;
-     if(odd==2 && sumofodd==2)
-     return true;
-     else if(odd<=1)
-     return true;
-
-     return false;
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };
