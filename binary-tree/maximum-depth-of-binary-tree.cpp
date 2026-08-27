63d991ff2e43ac6cbac7686107855aa5e8ed0b26
govindleo_11 <govindaragavan10@gmail.com>
2026-08-27 15:42:27 +0530

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/maximum-depth-of-binary-tree.cpp
+++ binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,23 +12,11 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        vector<vector<int>> ans;
-        if(!root) return 0;
+        if(root==NULL) return 0;
 
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            vector<int> level;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-                level.push_back(node->val);
-            }
-            ans.push_back(level);
-        }
-        return ans.size();
+        int lh=maxDepth(root->left);
+        int rh=maxDepth(root->right);
+
+        return 1 + max(lh,rh);
     }
 };
