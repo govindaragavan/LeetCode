035a4cdc3a7700abe2f882284d32a297d7e8e01b
govindleo_11 <govindaragavan10@gmail.com>
2026-08-28 22:56:05 +0530

    LC-maximum-width-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/maximum-width-of-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        int ans=0;
+        while(!q.empty()){
+            int n=q.size();
+            int left=q.front().second;
+            for(int i=0;i<n;i++){
+                auto p=q.front();
+                TreeNode* node=p.first;
+                int index=p.second;
+                q.pop();
+                if(i==n-1) ans=max(ans,index-left+1);
+                if(node->left) q.push({node->left,2*index+1});
+                if(node->right) q.push({node->right,2*index+2});
+            }
+            
+        }
+                    return ans;
+    }
+};
