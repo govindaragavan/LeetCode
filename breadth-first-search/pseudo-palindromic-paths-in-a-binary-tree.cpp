f79b98b72366225883ee39d0e06e221a2d33c4f6
govindleo_11 <govindaragavan10@gmail.com>
2026-08-28 22:00:59 +0530

    LC-pseudo-palindromic-paths-in-a-binary-tree [Runtime: N/A; Memory: N/A]

--- breadth-first-search/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ breadth-first-search/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,45 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int pseudoPalindromicPaths (TreeNode* root) {
-        vector<vector<int>> rtf;
-        helper(root,{},rtf);
-        int count=0;
-        for(auto q : rtf){
-         if(pseudopalindrome(q)) count++;
-        }
-        return count;
+    int pseudoPalindromicPaths(TreeNode* root) {
+        unordered_map<int, int> mp;
+        return helper(root, mp);
     }
-    private:
-    void helper(TreeNode*root,vector<int> temp,vector<vector<int>>& rtf){
-     if(!root) return;
-     temp.push_back(root->val);
-     if(!root->left && !root->right){
-         rtf.push_back(temp); 
-        return;}
-     helper(root->left,temp,rtf);
-     helper(root->right,temp,rtf);
-    }
-    bool pseudopalindrome(vector<int> a){
-        unordered_map<int ,int> mp;
-        for(int x : a)
-        mp[x]++;
-        int odd=0;
-        for(auto it=mp.begin();it!=mp.end();it++)
-        if(it->second%2!=0) odd++;
 
-        if(odd<=1) return 1;
-        return 0;
-        
+private:
+    int helper(TreeNode* root, unordered_map<int, int>& mp) {
+        if (!root) return 0;
+
+        mp[root->val]++;
+        if (!root->left && !root->right) {
+            int odd = 0;
+
+            for (auto it : mp) {
+                if (it.second % 2 != 0)
+                    odd++;
+            }
+
+            mp[root->val]--;   
+
+            return odd <= 1;
+        }
+
+        int ans = helper(root->left, mp)
+                + helper(root->right, mp);
+
+        mp[root->val]--;
+
+        return ans;
     }
 };
