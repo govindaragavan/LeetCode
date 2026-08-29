09ed18cf9f6d36af5bf00ee88ad8298f92dfde6c
govindleo_11 <govindaragavan10@gmail.com>
2026-08-29 15:40:15 +0530

    LC-maximum-sum-of-distinct-subarrays-with-length-k [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ sliding-window/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
+      unordered_map<int,int> mp;
+        int l=0,r=0,ans=0,sum=0;
+        while(l<=r && r<n){
+            sum+=nums[r];
 
-        unordered_map<int, int> mp;
-
-        int l = 0;
-        long long sum = 0;
-        long long ans = 0;
-
-        for (int r = 0; r < n; r++) {
-
-            mp[nums[r]]++;
-            sum += nums[r];
-
-            while (mp[nums[r]] > 1) {
+        mp[nums[r]]++;
+         while(mp[nums[r]]>1 && r<n){
                 mp[nums[l]]--;
-                sum -= nums[l];
-                l++;
-            }
+                sum-=nums[l]; 
+                l++;  
+        } 
+                
+            if(r-l+1==k){ 
+        ans=max(ans,sum); 
+        mp[nums[l]]--;
+        sum-=nums[l];      
+        l++; 
+          }
+            r++; 
 
-            if (r - l + 1 == k) {
-                ans = max(ans, sum);
-
-                mp[nums[l]]--;
-                sum -= nums[l];
-                l++;
-            }
         }
-
         return ans;
     }
 };
