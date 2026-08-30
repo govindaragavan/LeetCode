221cc407d523a207a3c6e026b5c4bb82c8456a7d
govindleo_11 <govindaragavan10@gmail.com>
2026-08-30 10:10:51 +0530

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- two-pointers/two-sum-ii-input-array-is-sorted.cpp
+++ two-pointers/two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        unordered_map<int,int> mp;
-        mp[0]=1;
+        map<int,int> mp;
         vector<int> ans;
         int n = numbers.size();
         int sum=0;
         for(int i=0;i<n;i++){
-            sum+=numbers[i];
-
-            if(mp.find(target-sum)!=mp.end()){
-                ans.push_back(mp[target-sum]);
-                ans.push_back(mp[sum]);
+                mp[numbers[i]]=i+1;
+            if(mp.find(target-numbers[i])!=mp.end() && target!=2*numbers[i]){
+                ans.push_back(mp[target-numbers[i]]);
+                ans.push_back(mp[numbers[i]]);
                 return ans;
         }
-        mp[sum]=i+1;
+
     }
     for(auto it=mp.begin();it!=mp.end();++it)
     cout<<it->first<<" ";
