6de1f542d5b86155958732eecd7fbf774e7be810
govindleo_11 <govindaragavan10@gmail.com>
2026-08-30 10:57:40 +0530

    LC-bag-of-tokens [Runtime: N/A; Memory: N/A]

--- array/bag-of-tokens.cpp
+++ array/bag-of-tokens.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         int n=tokens.size();
+        sort(tokens.begin(),tokens.end());
         int l=0,r=n-1;
-        int score;
+        int score=0;
+        int ans=0;
         while(l<=r){
         if(tokens[l]<=power)
-       {
-        power-=tokens[l];
-        score++;
+       {power-=tokens[l];
+      score++;
+        l++;
        }
+       else if(power<tokens[l])
+       {
+        power+=tokens[r];
+        score--;
+        r--;
+       }
+               ans=max(ans,score);
         }
-        return score;
+        return ans;
     }
 };
