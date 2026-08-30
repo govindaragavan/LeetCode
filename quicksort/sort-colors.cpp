9771ad2d7d687d60cbf2023aa4bf2cb2d520d411
govindleo_11 <govindaragavan10@gmail.com>
2026-08-30 10:19:14 +0530

    LC-sort-colors [Runtime: N/A; Memory: N/A]

--- quicksort/sort-colors.cpp
+++ quicksort/sort-colors.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-     sort(nums.begin(),nums.end());   
-    }
+        unordered_map<int,int> mp;
+        for(int x : nums)
+        mp[x]++;
+
+           nums.clear();
+            while(mp[0]>0){
+            nums.push_back(0);mp[0]--;}
+            while(mp[1]>0)
+            {nums.push_back(1);mp[1]--;}
+            while(mp[2]>0)
+            {nums.push_back(2);mp[2]--;}
+   }
 };
