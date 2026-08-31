f727075f13a231806c1aff32a63077063dad9589
govindleo_11 <govindaragavan10@gmail.com>
2026-08-31 19:28:27 +0530

    LC-find-bottom-left-tree-value [Runtime: 0 ms; Memory: 24.8 MB]

--- breadth-first-search/find-bottom-left-tree-value.cpp
+++ breadth-first-search/find-bottom-left-tree-value.cpp
@@ -6,36 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        if(!root) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<vector<int>> ans;
-        while(!q.empty()){
-            int n=q.size();
-            vector<int> level;
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                level.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            ans.push_back(level);
-        }
-        int m=ans.size();
-        return ans[m-1][0];
-    }
-    void solve(vector<int> &ans,TreeNode* root){
-        if(!root) return;
-        solve(ans,root->left);
-        solve(ans,root->right);
-                ans.push_back(root->val);
+    int value = -1;
+    int curr = -1;
 
+    void travel(TreeNode* root, int level) {
+        if (!root) {
+            return;
+        }
+
+        if (level > curr) {
+            curr = level;
+            value = root->val;
+        }
+
+        travel(root->left, level + 1);
+        travel(root->right, level + 1);
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        value = root->val;
+        travel(root, 0);
+        return value;
     }
 };
