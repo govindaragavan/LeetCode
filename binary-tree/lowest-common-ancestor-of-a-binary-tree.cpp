9b38037f49cd2be507aab7f3904134c6094171a7
govindleo_11 <govindaragavan10@gmail.com>
2026-09-01 15:52:33 +0530

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,40 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1;
-        vector<TreeNode*> path2;
+        if(root==NULL || root==q || root==p) return root;
 
-        solve(root, p, path1);
-        solve(root, q, path2);
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
 
-        int i = 0;
-
-        while (i < path1.size() && i < path2.size() &&
-               path1[i] == path2[i]) {
-            i++;
-        }
-
-        return path1[i - 1];
-    }
-
-    bool solve(TreeNode* root, TreeNode* target,
-               vector<TreeNode*>& path) {
-
-        if (!root)
-            return false;
-
-        path.push_back(root);
-
-        if (root == target)
-            return true;
-
-        if (solve(root->left, target, path))
-            return true;
-
-        if (solve(root->right, target, path))
-            return true;
-        path.pop_back();
-
-        return false;
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        else return root;
     }
 };
