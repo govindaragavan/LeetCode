c3cc8a62ba24656230a158261504ce6b9e354f76
govindleo_11 <govindaragavan10@gmail.com>
2026-09-01 14:59:09 +0530

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- depth-first-search/lowest-common-ancestor-of-a-binary-tree.cpp
+++ depth-first-search/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,38 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> set1;
-        vector<TreeNode*> set2;
+        vector<TreeNode*> path1;
+        vector<TreeNode*> path2;
 
-        solve(p,root,{},set1);
-        solve(q,root,{},set2);
-  for(TreeNode* x : set1) cout<<x->val<<" ";
-  cout<<endl;
-    for(TreeNode* x : set2) cout<<x->val<<" ";
-        int n=set1.size();
-        int m=set2.size();
-        int i=0;
-        TreeNode* node;
-        while(i<n && i<m){
-            if(set1[i]!=set2[i]) break; 
-           i++;            
+        solve(root, p, path1);
+        solve(root, q, path2);
+
+        int i = 0;
+
+        while (i < path1.size() && i < path2.size() &&
+               path1[i] == path2[i]) {
+            i++;
         }
-        return set1[i-1];
+
+        return path1[i - 1];
     }
-    void solve(TreeNode* p,TreeNode* root,vector<TreeNode*> curr,vector<TreeNode*> &set){
-        if(!root) return;
-        curr.push_back(root);
-        if(root==p){set=curr; return;}
-        solve(p,root->left,curr,set);
-        solve(p,root->right,curr,set);
+
+    bool solve(TreeNode* root, TreeNode* target,
+               vector<TreeNode*>& path) {
+
+        if (!root)
+            return false;
+
+        path.push_back(root);
+
+        if (root == target)
+            return true;
+
+        if (solve(root->left, target, path))
+            return true;
+
+        if (solve(root->right, target, path))
+            return true;
+        path.pop_back();
+
+        return false;
     }
 };
