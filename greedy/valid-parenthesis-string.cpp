d706bec17e354b59f9b3ec729e43cdbcf471ed61
govindleo_11 <govindaragavan10@gmail.com>
2026-09-02 15:00:38 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- greedy/valid-parenthesis-string.cpp
+++ greedy/valid-parenthesis-string.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     bool checkValidString(string s) {
         stack<char> st;
+        int c=0;
         int n=s.length();
         for(int i=0;i<n;i++){
             if(st.empty() || s[i]=='(')
             st.push(s[i]);
-            else if(s[i]==')' || s[i]=='*')
+            else if(s[i]==')')
             st.pop();
+            else if(s[i]=='*'){ c++; continue;}
             else return false;
-          
-
         }
-        return true;
+    
+int size=st.size();
+if(size<=c) return true;
+
+        return false;
     }
 };
