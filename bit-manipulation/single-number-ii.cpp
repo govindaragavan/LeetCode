8a9ebfd84a707962f8a6a48829a5c9ecfc4620f4
govindleo_11 <govindaragavan10@gmail.com>
2026-09-04 00:08:34 +0530

    LC-single-number-ii [Runtime: N/A; Memory: N/A]

--- bit-manipulation/single-number-ii.cpp
+++ bit-manipulation/single-number-ii.cpp
@@ -1,16 +1,16 @@
 class Solution {
- public:
-  int singleNumber(vector<int>& nums) {
-    int ans = 0;
-
-    for (int i = 0; i < 32; ++i) {
-      int sum = 0;
-      for (const int num : nums)
-        sum += num >> i & 1;
-      sum %= 3;
-      ans |= sum << i;
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]&1<<i) count++;
+            }
+            if(count%3==1)
+            ans=ans | 1<<i;
+        }
+        return ans;
     }
-
-    return ans;
-  }
 };
