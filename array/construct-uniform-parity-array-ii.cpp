c89c6c7a613b49a3906d2eb6346b3a355454e64b
govindleo_11 <govindaragavan10@gmail.com>
2026-09-03 22:24:50 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- array/construct-uniform-parity-array-ii.cpp
+++ array/construct-uniform-parity-array-ii.cpp
@@ -1,29 +1,10 @@
 class Solution {
-    vector<int> pse(vector<int> nums){
-        stack<int> st;
-        int n=nums.size();
-        vector<int> pse(n);
-        for(int i=0;i<n;i++){
-            if(!st.empty() && st.top()>=nums[i])
-            st.pop();
-
-            pse[i] = st.empty()? -1 : st.top();
-            
-            st.push(nums[i]);
-        }
-        return pse;
-    }
 public:
     bool uniformArray(vector<int>& nums1) {
-        int even=0,odd=0;
-                int n=nums1.size();
-        if(n==1) return true;
-        sort(nums1.begin(),nums1.end());
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0) even++;
-            else odd++;
-        }
-    if(odd==1 && nums1[n-1]%2==1) return false;
-        return 1;
+       int Min=*min_element(nums1.begin(),nums1.end());
+       if(Min%2==1) return 1;
+
+       for(int x : nums1) if(x%2==1) return false;
+       return true; 
     }
 };
