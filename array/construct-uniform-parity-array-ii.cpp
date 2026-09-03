ee6d4afa8690828a858267a94881d52a88609425
govindleo_11 <govindaragavan10@gmail.com>
2026-09-03 22:03:39 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- array/construct-uniform-parity-array-ii.cpp
+++ array/construct-uniform-parity-array-ii.cpp
@@ -18,11 +18,12 @@ public:
         int even=0,odd=0;
                 int n=nums1.size();
         if(n==1) return true;
+        sort(nums1.begin(),nums1.end());
         for(int i=0;i<n;i++){
             if(nums1[i]%2==0) even++;
             else odd++;
         }
-    if(odd==1) return false;
+    if(odd==1 && nums1[n-1]%2==1) return false;
         return 1;
     }
 };
