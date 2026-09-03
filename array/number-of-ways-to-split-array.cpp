baab12957eb442ece5ce840029c39baed47901b7
govindleo_11 <govindaragavan10@gmail.com>
2026-09-03 22:43:58 +0530

    LC-number-of-ways-to-split-array [Runtime: N/A; Memory: N/A]

---
+++ array/number-of-ways-to-split-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> prefix(n);
+int sum=0;
+for(int i=0;i<n;i++){
+    sum+=nums[i];
+    prefix[i]=sum;
+}  
+// prefix.insert(prefix.begin(),0);
+for(int x : prefix) cout<<x<<" ";  
+cout<<endl;
+int ans=0; 
+for(int i=0;i<n;i++){
+    if(prefix[i]>=prefix[n-1]-prefix[i]){ ans++; cout<<i<<" "<<prefix[i]<<" "<<prefix[n-1]-prefix[i]<<endl;}
+} 
+return ans-1;
+    }
+};
