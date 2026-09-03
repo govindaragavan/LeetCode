839b1e36784b41e30a9cdcc0348d0a7f1c5bd303
govindleo_11 <govindaragavan10@gmail.com>
2026-09-04 00:39:20 +0530

    LC-single-number-iii [Runtime: N/A; Memory: N/A]

--- bit-manipulation/single-number-iii.cpp
+++ bit-manipulation/single-number-iii.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++) ans=ans^nums[i];
+        int x=0;
+        for(int i=0;i<nums.size();i++) x=x^nums[i];
 
-        int rightmost=((ans&(ans-1))&ans);
+        int rightmost=x&-x;
         int b2=0,b1=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]&rightmost)
-            b2=b2^nums[i];
-            else
             b1=b1^nums[i];
+            else
+            b2=b2^nums[i];
         }
         return {b1,b2};
     }
