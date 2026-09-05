69a7882a2013af2124f05ca9dfca65d2ffa26ae7
govindleo_11 <govindaragavan10@gmail.com>
2026-09-05 13:17:48 +0530

    LC-rearrange-array-elements-by-sign [Runtime: N/A; Memory: N/A]

--- simulation/rearrange-array-elements-by-sign.cpp
+++ simulation/rearrange-array-elements-by-sign.cpp
@@ -2,42 +2,18 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
-
-        if (nums[0] < 0) {
-            int k = 1;
-            while (k < n) {
-                if (nums[k] > 0) {
-                    int i = k;
-                    while (i > 0) {
-                        swap(nums[i], nums[i - 1]);
-                        i--;
-                    }
-                    break;
-                }
-                k++;
+        vector<int> ans(n, 0);
+        int pos = 0, neg = 1;
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] >= 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            }
+            else {
+                ans[neg] = nums[i];
+                neg += 2;
             }
         }
-
-        for (int i = 1; i < n; i++) {
-            if ((nums[i - 1] > 0) == (nums[i] > 0)) {
-                int j = i + 1;
-
-                while (j < n) {
-                    if ((nums[i] > 0) != (nums[j] > 0)) {
-                        int k = j;
-
-                        while (k > i) {
-                            swap(nums[k], nums[k - 1]);
-                            k--;
-                        }
-
-                        break;
-                    }
-                    j++;
-                }
-            }
-        }
-
-        return nums;
+        return ans;
     }
 };
