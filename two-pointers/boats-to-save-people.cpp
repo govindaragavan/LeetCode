410ef16db5bb284cdd55dc375f0c68b60b9a4d45
govindleo_11 <govindaragavan10@gmail.com>
2026-09-05 19:37:48 +0530

    LC-boats-to-save-people [Runtime: 19 ms; Memory: 45.8 MB]

--- two-pointers/boats-to-save-people.cpp
+++ two-pointers/boats-to-save-people.cpp
@@ -1,38 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    int numRescueBoats(vector<int>& people, int limit)
-    {
-        int n = people.size();
-
-        if (n == 1)
-        {
-            return 1;
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int ans=0;
+        int l=0,r=people.size()-1;
+        while(l<=r){
+        ans++;
+        if(people[l]+people[r]<=limit){
+            l++;
+            r--;}
+        
+        else r--;
         }
-
-        int ans = 0;
-
-        int heavy = n - 1;
-        int light = 0;
-
-        sort(people.begin(), people.end());
-
-        while (light <= heavy)
-        {
-
-            ans++;
-
-            if (people[light] + people[heavy] <= limit)
-            {
-                light++;
-                heavy--;
-            }
-            else
-            {
-                heavy--;
-            }
-        }
-
+ 
         return ans;
     }
 };
