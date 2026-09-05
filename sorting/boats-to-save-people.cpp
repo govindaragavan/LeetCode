c5a417d321cf2bb3abeb19c8448191040ecff699
govindleo_11 <govindaragavan10@gmail.com>
2026-09-05 19:18:14 +0530

    LC-boats-to-save-people [Runtime: N/A; Memory: N/A]

---
+++ sorting/boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int ans=0;
+        int i=people.size()-1;
+        for(i=people.size()-1;i>=0;i--){
+            if(limit==people[i]) ans++;
+            else break;
+        }
+        int l=0,r=i;
+        while(l<r){
+        if(people[l]+people[r]<=limit){
+            ans++;
+            l++;
+            r--;
+        }
+        else break;
+        }
+        ans+=(r-l+1);
+        return ans;
+    }
+};
