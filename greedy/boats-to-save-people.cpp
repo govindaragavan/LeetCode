9805718d6b676eb334d4536a7e60d94d6db62c36
govindleo_11 <govindaragavan10@gmail.com>
2026-09-05 19:30:20 +0530

    LC-boats-to-save-people [Runtime: N/A; Memory: N/A]

--- greedy/boats-to-save-people.cpp
+++ greedy/boats-to-save-people.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
-        int ans=0;
-        int i=people.size()-1;
-        for(i=people.size()-1;i>=0;i--){
-            if(limit==people[i]) ans++;
+        int ans=0,n=people.size();
+       int i=0;
+       while(i<n){
+        int sum=0;
+        while(sum<=limit && i<n){
+            if(sum==limit) break;
+            if(sum+people[i]<=limit)
+            sum+=people[i];
             else break;
+            i++;
         }
-        if(i==-1) i=people.size()-1;
-        int l=0,r=i;
-        while(l<r){
-        if(people[l]+people[r]<=limit){
-            ans++;
-            l++;
-            r--;
-        }
-        else break;
-        }
-        ans+=(r-l+1);
+        ans++;
+              }       cout<<i;
         return ans;
     }
 };
