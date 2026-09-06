5115b2f645df2e0f2b3ce6754ae870b5745dad8b
govindleo_11 <govindaragavan10@gmail.com>
2026-09-06 12:13:29 +0530

    LC-majority-element-ii [Runtime: N/A; Memory: N/A]

--- boyer-moore-majority-vote-algorithm/majority-element-ii.cpp
+++ boyer-moore-majority-vote-algorithm/majority-element-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        int majority=floor((double)n/3);
+        int majority=n/3;
         unordered_map<int,int> map;
         set<int> set;
         for(int x : nums){
