ed1fd83a9a6c3b5553a4aa33ace06c4e21d476b9
govindleo_11 <govindaragavan10@gmail.com>
2026-09-06 12:00:25 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.cpp
+++ union-find/longest-consecutive-sequence.cpp
@@ -11,9 +11,9 @@ if(set.size()==1) return 1;
 int ans=0,l=1;
 for(auto it=set.begin();it!=set.end();it++){
     cout<<*it<<endl;
+    ans=max(ans,l);
     if(set.find(*it + 1)!=set.end()) l++;
     else l=0;
-    ans=max(ans,l);
 }
         return ans;
     }
