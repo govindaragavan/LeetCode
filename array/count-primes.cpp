d6ebb6c51a950920aeda24cf8daf8ac3971605e3
govindleo_11 <govindaragavan10@gmail.com>
2026-09-06 23:06:53 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- array/count-primes.cpp
+++ array/count-primes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countPrimes(int n) {
         map<int,int> map;
-        if(n<=2) return 0;
+        if(n<=3) return 0;
         for(int i=2;i*i<=n;i++){
             if(n%i==0)
             map[i]++;
