e329fcad207aa107386c9db010e142c78cea89c3
govindleo_11 <govindaragavan10@gmail.com>
2026-09-07 00:23:35 +0530

    LC-the-kth-factor-of-n [Runtime: N/A; Memory: N/A]

---
+++ math/the-kth-factor-of-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> ans;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+            ans.push_back(i);
+
+            if(i!=n/i)
+            ans.push_back(n/i);}
+        }
+        sort(ans.begin(),ans.end());
+        for(int x : ans) cout<<x<<" ";
+        if(ans.size()<k) return -1;
+        return ans[k-1];
+    }
+};
