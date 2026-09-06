b79c1827b6c9bb8277febe8d3ea71420c3a5ca5c
govindleo_11 <govindaragavan10@gmail.com>
2026-09-06 23:34:16 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- sieve-theory/count-primes.cpp
+++ sieve-theory/count-primes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        map<int,int> map;
-        if(n<=2) return 0;
-        if(n==3) return 1;
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)
-            map[i]++;
+              if (n <= 2) return 0;
+        vector<int> a(n, 1);
+        a[0] = a[1] = 0;
 
-            n/=i;
+      for(int i=2;i*i<n;i++){
+        if(a[i]==1){
+            for(int j=i*i;j<n;j+=i){
+                a[j]=0;
+            }
         }
-        if(n>1) map[n]++;
-        int ans=1;
-        for(auto it=map.begin();it!=map.end();it++) ans*=(it->second+1);
+      }
+      int ans=0;
+      for(int i=2;i<n;i++) if(a[i]) ans++;
         return ans;
     }
 };
