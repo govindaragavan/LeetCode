6197c8b19f257a724bdd9a1148bb5a73078e6e46
govindleo_11 <govindaragavan10@gmail.com>
2026-09-08 10:24:51 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.cpp
+++ dynamic-programming/house-robber.cpp
@@ -1,13 +1,15 @@
 class Solution {
+    void solve(vector<int> a,int curr,int &sum,int i){
+         curr+=a[i];
+        sum=max(sum,curr); 
+         if(a.size()>i+2){
+            for(int j=i+2;j<a.size();j++) solve(a,curr,sum,j);
+         }
+    }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        long long sum1=0;
-        for(int i=0;i<n;i+=2)
-            sum1+=nums[i];
-            long long sum2=0;
-            for(int i=1;i<n;i+=2)
-            sum2+=nums[i];
-        return max(sum1,sum2);
+int sum=0;
+solve(nums,0,sum,0);
+return sum;
     }
 };
