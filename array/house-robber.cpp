b93efffeebf6fed85a32cc21a3ba4df6f4325c2b
govindleo_11 <govindaragavan10@gmail.com>
2026-09-08 10:56:08 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- array/house-robber.cpp
+++ array/house-robber.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    int solve(vector<int> nums,int i){
+    int solve(vector<int> nums,int i,vector<int> &dp){
         if (i >= nums.size())
             return 0;
+        if(dp[i]!=-1) return dp[i];
+        int rob = nums[i] + solve(nums, i + 2,dp);
+        int skip = solve(nums, i + 1,dp);
 
-        int rob = nums[i] + solve(nums, i + 2);
-        int skip = solve(nums, i + 1);
-
-        return  max(rob, skip);
+        return dp[i]=max(rob, skip);
     }
 public:
     int rob(vector<int>& nums) {
-int ans=solve(nums,0);
+        vector<int> dp(nums.size()+1,-1);
+int ans=solve(nums,0,dp);
 return ans;
     }
 };
