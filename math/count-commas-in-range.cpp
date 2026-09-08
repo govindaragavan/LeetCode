8d7f3fe4e52a11a58201a1863d922344cd453a1b
govindleo_11 <govindaragavan10@gmail.com>
2026-09-08 09:59:17 +0530

    LC-count-commas-in-range [Runtime: 0 ms; Memory: 8.4 MB]

--- math/count-commas-in-range.cpp
+++ math/count-commas-in-range.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-       int len=to_string(n).length() ;
-       if(len<4 || n<1000) return 0;
+       if( n<1000) return 0;
        else return n-999;
 
     }
