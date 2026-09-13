b3d32faba7fb001da0f2b2f15ac7e3d8116abf27
govindleo_11 <govindaragavan10@gmail.com>
2026-09-13 11:09:54 +0530

    LC-closest-prime-numbers-in-range [Runtime: N/A; Memory: N/A]

---
+++ sieve-theory/closest-prime-numbers-in-range.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> closestPrimes(int left, int right) {
+        vector<int> v;
+        for(int i=left;i<=right;i++){
+            if(isPrime(i)) v.push_back(i);
+        }
+        for(int x : v) cout<<x<<" ";
+        if(v.size()<2) return {-1,-1};
+        int diff=v[1]-v[0];
+        vector<int> ans={v[0],v[1]};
+        for(int i=2;i<v.size();i++){
+            if(v[i]-v[i-1]<diff){
+                diff=v[i]-v[i-1];
+                ans={v[i-1],v[i]};
+            }
+        }
+        return ans;
+    }
+    private:
+    bool isPrime(int n){
+        if(n<=1) return false;
+        if(n==2) return true;
+        if(n%2==0) return false;
+        for(int i=3;i<=sqrt(n);i+=2){
+      if(n%i==0) return false;
+        }
+        return true;
+    }
+};
