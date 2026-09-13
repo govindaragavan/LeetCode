de0ffdf0cfbfe882889640972d1e69363ea6396b
govindleo_11 <govindaragavan10@gmail.com>
2026-09-13 10:53:07 +0530

    LC-distinct-prime-factors-of-product-of-array [Runtime: N/A; Memory: N/A]

--- prime-factorization/distinct-prime-factors-of-product-of-array.cpp
+++ prime-factorization/distinct-prime-factors-of-product-of-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-        long long product=1;
-        for(int x : nums) product*=x;
-        int ans=solve(product);
-        return ans;
+        __int128 product=1;
+        unordered_map<int,int> map;
+        for(int x : nums) solve(x,map);
+        return map.size();
     }
     private:
-    int solve(long long n){
-       unordered_map<int,int> map;
+    void solve(long long n, unordered_map<int,int> &map){
           for(int i=2;i*i<=n;i++){
             while(n%i==0){
                 map[i]++;
@@ -16,6 +15,5 @@ public:
             }
           }
           if(n>1) map[n]++;
-          return map.size();
     }
 };
