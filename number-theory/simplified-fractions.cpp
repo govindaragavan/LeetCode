28b4bf2fe719cac26fc218b3514588f67b919778
govindleo_11 <govindaragavan10@gmail.com>
2026-09-13 14:59:04 +0530

    LC-simplified-fractions [Runtime: N/A; Memory: N/A]

--- number-theory/simplified-fractions.cpp
+++ number-theory/simplified-fractions.cpp
@@ -16,7 +16,8 @@ public:
     for(int i=2;i<n;i++){
         for(int j=i+1;j<=n;j++){
             if(__gcd(i,j)==1){
-                            v.push_back(to_string(i)+"/"+to_string(j));}
+            string s=to_string(i)+"/"+to_string(j);
+            v.push_back(s);}
         }
     }
     return v;
