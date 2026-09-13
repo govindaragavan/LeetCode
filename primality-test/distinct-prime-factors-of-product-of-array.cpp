4dc8d17f4fce643cb19ed581bae9588dae741f92
govindleo_11 <govindaragavan10@gmail.com>
2026-09-13 10:54:46 +0530

    LC-distinct-prime-factors-of-product-of-array [Runtime: N/A; Memory: N/A]

--- primality-test/distinct-prime-factors-of-product-of-array.cpp
+++ primality-test/distinct-prime-factors-of-product-of-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-        __int128 product=1;
-        unordered_map<int,int> map;
+        unordered_set<int> map;
         for(int x : nums) solve(x,map);
         return map.size();
     }
     private:
-    void solve(long long n, unordered_map<int,int> &map){
+    void solve(long long n, unordered_set<int> &map){
           for(int i=2;i*i<=n;i++){
             while(n%i==0){
-                map[i]++;
+                map.insert(i);
                 n/=i;
             }
           }
-          if(n>1) map[n]++;
+          if(n>1) map.insert(n);
     }
 };
