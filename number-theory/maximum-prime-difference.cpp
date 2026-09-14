924ae2ee0ddfbba28c63b4d9228b04a8b33668c6
govindleo_11 <govindaragavan10@gmail.com>
2026-09-14 05:41:06 +0530

    LC-maximum-prime-difference [Runtime: N/A; Memory: N/A]

--- number-theory/maximum-prime-difference.cpp
+++ number-theory/maximum-prime-difference.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int maximumPrimeDifference(vector<int>& nums) {
-        int n=nums.size();
-        int low=-1,high=-1;
-        for(int i=0;i<n;i++){
-        if(isPrime(nums[i])) {low=i; break;}
+    bool isprime(int n){
+        if(n<2) return false;
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0) return false;
         }
-        for(int i=n-1;i>=0;i--){
-        if(isPrime(nums[i])) {high=i; break;}
-        }
-        if(low!=-1 && high!=-1) return abs(high-low);
-
-        return 0;
+        return true;
     }
-    bool isPrime(int n){
-        if(n<=1) return 0;
-        if(n==2) return 1;
-        if(n%2==0) return 0;
-
-        for(int i=3;i<=sqrt(n);i++)
-            if(n%i==0) return 0;
-
-            return 1;
+    int maximumPrimeDifference(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int first = 0, last = 0, n = nums.size();
+        for(int i=0; i<n; i++){
+            if(isprime(nums[i])){
+                first = i; break;
+            }
+        }
+        for(int i=n-1; i>=0; i--){
+            if(isprime(nums[i])){
+                last = i; break;
+            }
+        }
+        return last-first;
     }
 };
