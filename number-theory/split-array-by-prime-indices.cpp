e56501dbf539cd082a2e8ca5e16c7232ff82a938
govindleo_11 <govindaragavan10@gmail.com>
2026-09-14 05:57:14 +0530

    LC-split-array-by-prime-indices [Runtime: N/A; Memory: N/A]

--- number-theory/split-array-by-prime-indices.cpp
+++ number-theory/split-array-by-prime-indices.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     long long splitArray(vector<int>& nums) {
-        int n=nums.size();
-        long long OtherSum=0;
-    long long PrimeSum=0;
-        for(int x=0;x<n;x++){ 
-        if(isPrime(x)) PrimeSum+=nums[x];
-        else OtherSum+=nums[x];}
+        long long sum=0;
+        for(int x : nums) sum+=x;
+    long long Primesum=0;
+        for(int x : nums) 
+        if(isPrime(x)) Primesum+=x;
 
-    return abs(PrimeSum-OtherSum);
+        cout<<sum<<" "<<Primesum;
+    return abs((2*Primesum)-sum);
         
     }
 
     bool isPrime(int n){
         if(n<2) return  0;
-        for(int i=2;i<=sqrt(n);i++)
+                if(n==2) return 1;
+
+        if(n%2==0) return 0;
+        for(int i=3;i<=sqrt(n);i++)
         if(n%i==0) return 0;
 
         return 1;
