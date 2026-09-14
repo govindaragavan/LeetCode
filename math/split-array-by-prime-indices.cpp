ed0a8639730a96ef3c626c30c0c99681ec955608
govindleo_11 <govindaragavan10@gmail.com>
2026-09-14 05:56:28 +0530

    LC-split-array-by-prime-indices [Runtime: N/A; Memory: N/A]

--- math/split-array-by-prime-indices.cpp
+++ math/split-array-by-prime-indices.cpp
@@ -14,9 +14,7 @@ public:
 
     bool isPrime(int n){
         if(n<2) return  0;
-        if(n==2) return 1;
-        if(n%2==0) return 0;
-        for(int i=3;i<=sqrt(n);i++)
+        for(int i=2;i<=sqrt(n);i++)
         if(n%i==0) return 0;
 
         return 1;
