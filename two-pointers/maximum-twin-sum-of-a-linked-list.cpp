864dc98b2caaca995970184b0994461dcab85402
govindleo_11 <govindaragavan10@gmail.com>
2026-09-19 11:18:42 +0530

    LC-maximum-twin-sum-of-a-linked-list [Runtime: N/A; Memory: N/A]

--- two-pointers/maximum-twin-sum-of-a-linked-list.cpp
+++ two-pointers/maximum-twin-sum-of-a-linked-list.cpp
@@ -11,19 +11,39 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ListNode* temp=head;
-vector<int> v;
-        while(temp){
-            v.push_back(temp->val);
-            temp=temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int l=0,r=v.size()-1;
-        int ans=0;
-        while(l<r){
-      ans=max(ans,v[l]+v[r]);
-      l++;
-      r--;
+
+        ListNode* prev = NULL;
+        ListNode* cur = slow;
+
+        while(cur!= NULL)
+        {
+            ListNode* next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;  
         }
-        return ans;
+
+        ListNode* newcur = head;
+        ListNode* backcur = prev;
+        int cursum = 0;
+        int maxsum = 0;
+
+        while(newcur != slow)
+        {
+            cursum = newcur->val + backcur->val;
+            maxsum = max(maxsum, cursum);
+            newcur=newcur->next;
+            backcur=backcur->next;
+        }
+
+        return maxsum;
     }
 };
