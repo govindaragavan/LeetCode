36bb7b7fa48d77daaba70f1a3dbdb3a838a44a64
govindleo_11 <govindaragavan10@gmail.com>
2026-09-20 09:41:06 +0530

    LC-remove-nodes-from-linked-list [Runtime: N/A; Memory: N/A]

--- linked-list/remove-nodes-from-linked-list.cpp
+++ linked-list/remove-nodes-from-linked-list.cpp
@@ -1,29 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-       ListNode* dummy=new ListNode(head->val);
-       dummy->next=head;
-        ListNode* temp =head;
-       ListNode* prev=dummy;
-ListNode* prev1=dummy;
-      while(temp){
-        if(temp->val>prev1->val){
-           prev->next=temp;
-           prev=temp;
+        ListNode* cur = head;
+        stack<ListNode*> stack;
+        
+        while (cur != nullptr) {
+            while (!stack.empty() && stack.top()->val < cur->val) {
+                stack.pop();
+            }
+            stack.push(cur);
+            cur = cur->next;
         }
-        temp=temp->next;
-        prev1=prev1->next;
-      }
-      return dummy->next;
+        
+        ListNode* nxt = nullptr;
+        while (!stack.empty()) {
+            cur = stack.top();
+            stack.pop();
+            cur->next = nxt;
+            nxt = cur;
+        }
+        
+        return cur;
     }
 };
